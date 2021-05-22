c7a6a4c3cea749ddeddb6ee628a9c8ed0db40f90
Damian <pluskwadamian@interia.pl>
2021-05-22 15:55:27 +0200

    dodanie pliku

--- projekt1/maths/views.py
+++ projekt1/maths/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def div(request, x, y):
+    return HttpResponse(f'div = {x / y}')
+
+
+def mul(request, x, y):
+    return HttpResponse(f'div = {x * y}')
