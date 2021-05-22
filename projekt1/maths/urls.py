1410f792d3b8a053e153d2a9ee98727a7439ebf6
Damian <pluskwadamian@interia.pl>
2021-05-22 16:08:14 +0200

    urls

--- projekt1/maths/urls.py
+++ projekt1/maths/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.div, name='div'),
+    path('', views.mul, name='mul'),
+    path('', views.add, name='add'),
+    path('', views.sub, name='sub'),
 ]
